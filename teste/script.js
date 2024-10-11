9ba0170f0ba5c15a762649029d1e66a4e3a73e0f
Heloisa Tanaka <helo.l.tanaka@gmail.com>
2024-10-11 10:47:12 -0300

    texto em cima do inimigo

--- teste/script.js
+++ teste/script.js
@@ -1,30 +1,32 @@
 const levi = document.querySelector('.levi');
 const enemie = document.querySelector('.enemie');
+const enemyText = document.getElementById('enemy-text'); // Selecione o texto
 const vidas = 5;
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         levi.src = 'imagens/levi1.gif';
     } else if (!isNaN(event.key)) { 
-        if (levi.src.endsWith('levi1.gif' || 'levidano.gif')) {
+        if (levi.src.endsWith('levi1.gif') || levi.src.endsWith('levidano.gif')) {
             levi.src = 'imagens/leviataque.gif'; 
-
-            }
-        } 
+        }
+    }
 });
 
 const loop = setInterval(() => {
-        
     const enemiePosition = enemie.offsetLeft;
-    const leviPosition = levi.offsetLeft
+    const leviPosition = levi.offsetLeft;
+    enemyText.style.left = (enemiePosition + 100) + 'px';
+    enemyText.style.top = (enemie.offsetTop - 20) + 'px'; // Ajusta a posição do texto
+
+       
     if (enemiePosition <= leviPosition) {        
         levi.src = 'imagens/estatico.png'; 
         setTimeout(() => {
             levi.src = 'imagens/levi1.gif';
-        }, 1350);        
+        }, 2000);     /*1350*/   
     }
-}, 500);
-
+}, 0); /*500*/
 
 
 
